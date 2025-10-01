1249c4cf33d0373945d63361fbe292cfdd2513d8
omurilolima <muriloserafimlima@gmail.com>
2025-10-01 15:08:44 +0100

    Button, Alert and StyleSheet

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,21 +1,25 @@
-import { StyleSheet, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, Text, Image} from 'react-native';
+import { StyleSheet, Alert, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, Text, Image, Button} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TouchableHighlight onPress={() => console.log('Image tapped!')}>
-        <Image source={{
-          uri: 'https://cdn-icons-png.flaticon.com/128/3845/3845894.png',
-          width: 100,
-          height: 100,
-        }}/>
-      </TouchableHighlight>
+      <Button 
+        title="Click me" 
+        color="#841584"
+        onPress={() => Alert.alert('My title', 'My message', [
+          {text: 'Yes', onPress: () => console.log('Yes')},
+          {text: 'No', onPress: () => console.log('No')}
+        ])} 
+      />
     </SafeAreaView>
   );
 }
 
+// It is a good practice to define styles outside of the component
+// to avoid re-creating the styles on each render
+// Also, it is a good practice to use StyleSheet.create to create styles
+// because it provides better performance and better error checking
 const styles = StyleSheet.create({
   container: {
     flex: 1,

cf744ac86c3316d5b8459bbdb204abca438843cd
omurilolima <muriloserafimlima@gmail.com>
2025-10-07 13:49:46 +0100

    Layout - Detecting Orientation Changes

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,30 +1,29 @@
-import { StyleSheet, Alert, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, Text, Image, Button} from 'react-native';
+import { View, StyleSheet, Alert, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, Text, Image, Button, Platform, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+
+// install npm i @react-native-community/hooks
+// change the app.json file to set the orientation to "default" to enable both orientations
 
 export default function App() {
+  const {landscape} = useDeviceOrientation(); // returns an object with landscape and portrait boolean values
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Button 
-        title="Click me" 
-        color="#841584"
-        onPress={() => Alert.alert('My title', 'My message', [
-          {text: 'Yes', onPress: () => console.log('Yes')},
-          {text: 'No', onPress: () => console.log('No')}
-        ])} 
-      />
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: '100%',
+          height: landscape ? '100%' : '30%', // if landscape, take full height, else take 30% height
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
 
-// It is a good practice to define styles outside of the component
-// to avoid re-creating the styles on each render
-// Also, it is a good practice to use StyleSheet.create to create styles
-// because it provides better performance and better error checking
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
